4198cd83c9a941f80becdd26782d136693dbb3a1
Gabriel Salazar <salazarsierrajose@gmail.com>
2023-04-25 14:58:52 -0500

    "First web component carousel try, it didnt go well"

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 
     document.querySelector('.nav-menu').classList.toggle('show')
 
-
 } )
+
+document.body.style.overflowX = "hidden";
+
+const images = [
+    {img: "https://assets.static-gm.com/Assets/63a90616-1fa5-40c9-814a-17f4de353956/a7e32d73-c4aa-4142-a0f6-0cde1af569e9/v-1645558907/Desktop.webp", alt : "ONIX"},
+    {img: "https://www.valleychevy.com/wp-content/uploads/2021/11/color-picker_0001_22CHSpark-Summit-White.png", alt : "SPARK"},
+    {img: "https://assets.static-gm.com/Assets/925e467e-b354-4c6a-bee7-094286fb63d9/6905cfd6-7a9e-426a-9544-7e1a634c8c8e/v-1621478869/Desktop.png", alt : "CAMARO ZL1"},
+    {img: "https://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2023/trucks/silverado-ld/01-images/trims/png/2023-silverado1500-cc10543-1lt-gxd-trimselector.png?imwidth=960", alt : "SILVERADO"},
+
+]
+
+const carousel = document.querySelector("my-carousel")
+carousel.giveImages(images)
 
 
 
