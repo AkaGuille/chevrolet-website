960531e7dd580eb53442637d3d64615b9ace50e0
Gabriel Salazar <salazarsierrajose@gmail.com>
2023-04-28 00:14:52 -0500

    "28/04/2023 ajustes en el product-detail"

--- product-detail/product-component.js
+++ product-detail/product-component.js
@@ -22,11 +22,54 @@ class productComponent extends HTMLElement {
         this.innerHTML = `
         <link rel="stylesheet" href="/product-detail/style.css">
         <img src= ${this.image} alt="">
-        <h2>${this.name}</h2>
-        <p>$ ${this.price}</p>
-        <p>${this.description}</p>
-        <p>${this.category}</p>
-        <p>${this.color}</p>
+
+        <header>
+        <!--Navegacion-->
+        <nav class="nav-main">
+
+            <div class="menu-btn">
+                
+            </div>
+            <!--Logo debo cambiarlo, si puedes ayudame a buscarlo en web-->
+            <!--Navegacion-->
+
+            <ul class="nav-menu2">
+                <li id="location-text">
+                    <a href="#">ABOUT ${this.name}
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">PRICES AND VERSIONS</a>
+                </li>
+                <li>
+                    <a href="#">ACCESSORIES</a>
+                </li>
+
+                <li>
+                    <a href="#">CONTACT</a>
+                </li>
+
+
+            </ul>
+
+        </nav>
+
+    </header>
+        <h2 id="car-title" style="position: absolute; top: 90%; transform: translate(-50%, -50%);">${this.name}</h2>
+        <p id="car-price">BASE PRICE: ${this.price}</p>
+        <p id="car-description">${this.description}</p>
+        <p id="car-category">TYPE: ${this.category}</p>
+        <p id="car-color">COLOR: ${this.color}</p>
+        <p id="car-color"> ${this.name} INTERIOR </p>
+
+        <div class="grid-container">
+        <img src="https://www.chevrolet.com.sv/wp-content/uploads/2021/03/1920x1080-album-3.png" alt="Imagen 1">
+        <img src="https://img.automexico.com/2020/01/17/2gS0vPdI/spark20-5-7707.jpg" alt="Imagen 2">
+        <img src="https://img.automexico.com/2018/10/11/b09YwtPD/sillas-de-cuero-01-ded7.jpeg" alt="Imagen 3">
+        <img src="https://img.automexico.com/2018/10/11/b09YwtPD/sillas-de-cuero-c2fc.png" alt="Imagen 4">
+        </div>
+
     `
     }
 
